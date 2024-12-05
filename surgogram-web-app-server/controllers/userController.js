884d94b7f3b85f424673547a68ad37e81a80c445
Nethmi Rathnayake <108873206+NethmiR@users.noreply.github.com>
2024-12-06 04:16:40 +0530

    feat/added-authentication-check-to-privilaged-controllers

--- surgogram-web-app-server/controllers/userController.js
+++ surgogram-web-app-server/controllers/userController.js
@@ -1,4 +1,5 @@
 const userService = require('../services/userService');
+const { authenticateUser } = require('../middleware/authMiddleware');
 
 exports.createUser = async (req, res) => {
     try {
@@ -10,14 +11,17 @@ exports.createUser = async (req, res) => {
     }
 };
 
-exports.updateUser = async (req, res) => {
-    try {
-        const userId = req.params.id;
-        const userData = req.body;
-        const updatedUser = await userService.updateUser(userId, userData);
-        res.status(200).json(updatedUser);
-    } catch (error) {
-        res.status(400).json({ error: error.message });
+exports.updateUser = [
+    authenticateUser,
+    async (req, res) => {
+        try {
+            const userId = req.params.id;
+            const userData = req.body;
+            const updatedUser = await userService.updateUser(userId, userData);
+            res.status(200).json(updatedUser);
+        } catch (error) {
+            res.status(400).json({ error: error.message });
+        }
     }
-};
+];
 
