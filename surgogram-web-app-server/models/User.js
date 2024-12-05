b4c649c0cfdaf205606b16ff97a71c5e2cee59a6
Nethmi Rathnayake <108873206+NethmiR@users.noreply.github.com>
2024-12-05 15:59:21 +0530

    feat/user- created user model

---
+++ surgogram-web-app-server/models/User.js
@@ -0,0 +1,30 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../sequelize');
+
+const User = sequelize.define('User', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    fullName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    userName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+    },
+    profileUrl: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    }
+});
+
+module.exports = User;
