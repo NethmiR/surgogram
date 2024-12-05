7f31eab88aced678e7422decd7f7f2e33149400f
Nethmi Rathnayake <108873206+NethmiR@users.noreply.github.com>
2024-12-06 02:08:54 +0530

    feat/created-authServices

--- surgogram-web-app-server/storageService.js
+++ surgogram-web-app-server/storageService.js
@@ -6,34 +6,41 @@ import sequelize from './sequelize';
 const ALLOWED_EXTENSIONS = ['jpg', 'jpeg', 'png'];
 
 async function uploadImage(bucketName, file) {
-    const extension = path.extname(file.originalname).toLowerCase().substring(1);
-    if (!ALLOWED_EXTENSIONS.includes(extension)) {
-        throw new Error('Invalid file type. Only JPG, JPEG, and PNG are allowed.');
+    try {
+        const extension = path.extname(file.originalname).toLowerCase().substring(1);
+
+        if (!ALLOWED_EXTENSIONS.includes(extension)) {
+            throw new Error('Invalid file type. Only JPG, JPEG, and PNG are allowed.');
+        }
+
+        //generate a unique filename
+        const fileName = `${uuidv4()}.${extension}`;
+
+        const { data, error } = await supabase.storage
+            .from(bucketName)
+            .upload(fileName, file.buffer, {
+                contentType: file.mimetype,
+            });
+
+        if (error) {
+            throw new Error(`Failed to upload image: ${error.message}`);
+        }
+
+        const url = `${process.env.SUPABASE_URL}/storage/v1/object/public/${bucketName}/${fileName}`;
+
+        // await saveImageUrlToDatabase(url);
+        return url;
+    } catch (error) {
+        console.error('Error uploading image:', error);
+        throw error;
     }
-
-    const fileName = `${uuidv4()}.${extension}`;
-    const { data, error } = await supabase.storage
-        .from(bucketName)
-        .upload(fileName, file.buffer, {
-            contentType: file.mimetype,
-        });
-
-    if (error) {
-        throw new Error(`Failed to upload image: ${error.message}`);
-    }
-
-    const url = `${process.env.SUPABASE_URL}/storage/v1/object/public/${bucketName}/${fileName}`;
-
-    await saveImageUrlToDatabase(url);
-
-    return url;
 }
 
-async function saveImageUrlToDatabase(url) {
-    await sequelize.query('UPDATE "User" SET "URL" = $1 WHERE id = $2', {
-        bind: [url, userId],
-        type: sequelize.QueryTypes.UPDATE,
-    });
-}
+// async function saveImageUrlToDatabase(url) {
+//     await sequelize.query('UPDATE "User" SET "URL" = $1 WHERE id = $2', {
+//         bind: [url, userId],
+//         type: sequelize.QueryTypes.UPDATE,
+//     });
+// }
 
 export { uploadImage };
