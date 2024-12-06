ccd5b588400ad2e44242594ab92489879c9332c0
Nethmi Rathnayake <108873206+NethmiR@users.noreply.github.com>
2024-12-06 13:10:12 +0530

    feat/client- initialized the project

---
+++ surgogram-web-app-client/src/pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="bg-blue-500 h-screen flex items-center justify-center">
+      <h1 className="text-red-500 text-4xl font-bold">Hello World</h1>
+    </div>
+  );
+}
